e9318767822fb48ddec41b8184df5062279b518f
Manu Joseph <vmanujoseph@gmail.com>
2023-05-20 22:52:57 +0530

    comments

--- inverted_pendulum_controller/src/goal2.py
+++ inverted_pendulum_controller/src/goal2.py
@@ -1,18 +1,17 @@
-#! /usr/bin/env python3
-
+# neccessary packages
 import rospy                             
 from inverted_pendulum_sim.msg import ControlForce        
 from math import sin, pi
 
-rospy.init_node('goal2')         
-pub = rospy.Publisher('/inverted_pendulum/control_force', ControlForce, queue_size=10)                              
+rospy.init_node('goal2')  #node       
+pub = rospy.Publisher('/inverted_pendulum/control_force', ControlForce, queue_size=10)  #publisher                            
 
-rate = rospy.Rate(100)              
+rate = rospy.Rate(100)   #frequency of node set to 100 hz           
 force_obj = ControlForce()                            
 force_obj.force = 0
-frequency = 10   
-amplitude = 30                 
-t = 0
+frequency = 10   #frequncy
+amplitude = 30   #amplitude               
+t = 0 #time
 
 while not rospy.is_shutdown():             
   pub.publish(force_obj)
